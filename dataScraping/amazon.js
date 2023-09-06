8be20fada02808b3baa37e4d687528f8ae1471a6
Dinesh kumar <dineshsoni8569@gmail.com>
2023-09-06 16:07:11 +0530

    amazon scrapper completed

--- dataScraping/amazon.js
+++ dataScraping/amazon.js
@@ -1,81 +1,17 @@
-const getAmazonProduct = () => {
-  const data = document.querySelectorAll('.s-result-item');
-  const res = [];
-  data.forEach((card,i)=>{
-      const title = card.querySelector('.a-section.a-size-mini.a-spacing-none.a-color-base.s-line-clamp-2')?.textContent;
-      const imgElem = card.querySelector('.s-image');
-      const imgSrc = imgElem?.getAttribute ? imgElem.getAttribute('src')  :""; 
+    const getAmazonProduct = () => {
+      const data = document.querySelectorAll('.s-result-item');
+      const res = [];
+      data.forEach((card,i) =>{
+        const title = card.querySelector('.a-size-mini.a-spacing-none.a-color-base.s-line-clamp-2')?.textContent;
+        const imgEl = card.querySelector('.s-image');
+        const imgSr = imgEl?.getAttribute ? imgEl?.getAttribute('src') :"";
+        const priceEl = card.querySelector('.a-price')?.textContent;
+        const price = priceEl?.length>1? priceEl?.split("₹",2):" ";
+        const p = price.length>1? "₹"+ price[1]:"₹___";
+        res[i]=  {title,imgSr,"price":p}
+        
+    })
+        return res;
+      };
+      getAmazonProduct();
 
-      res[i] = {
-        "title":title,
-        "image":imgSrc
-      }
-    })  
-    return res;
-  };
-  getAmazonProduct();
-
-  
-  // const price = card.querySelector('.a-price.a-price-whole').textContent;
-  // const price = card.querySelector('.a-size-base.a-link-normal.s-no-hover.s-underline-text.s-underline-link-text.s-link-style.a-text-normal.a-offscreen').textContent;
-  // const price = card.querySelector('.puis-price-instructions-style.a-text-normal.a-offscreen').textContent;
-const img = getImg();
-const getImg = () => {
-  const imgs = document.querySelectorAll(".s-image");
-  imgs.forEach((img) => console.log(img.currentSrc));
-  const arr = [...imgs];
-  const url = arr.map((x) => x.src);
-  console.log(imgs);
-};
-const getTitle = () => {
-  const titles = document.querySelectorAll(
-    ".a-size-base-plus.a-color-base.a-text-normal"
-  );
-  titles.forEach((val) => console.log(val.innerText));
-  const arr = [...titles];
-  const title = arr.map((x) => x.innerText);
-  console.log(title);
-};
-getTitle();
-const getPrice = () => {
-  const prices = document.querySelectorAll(
-    ".a-row.a-size-base.a-color-base.a-size-base.a-link-normal.s-no-hover.s-underline-text.s-underline-link-text.s-link-style.a-text-normal.a-price.a-offscreen"
-  );
-  prices.forEach((val) => console.log(val.innerText));
-  const arr = [...prices];
-  // const url = arr.map(x=> x.src)
-  // console.log(titles);
-};
-getPrice();
-console.log(prices);
-const allCard = document.querySelectorAll("[data-asin]");
-document
-  .querySelectorAll(".a-row.a-size-base.a-color-base a")[0]
-  .innerText.split(" ", 1);
-//const cs= document.querySelectorAll('.s-card-container.s-overflow-hidden.aok-relative.puis-expand-height.puis-include-content-margin.puis.puis-v2jxz9i2qjba5p2azxbyo8sltz5.s-latency-cf-section.s-card-border')
-// class=".s-card-container.s-overflow-hidden.aok-relative.puis-expand-height.puis-include-content-margin.puis.puis-v2jxz9i2qjba5p2azxbyo8sltz5.s-latency-cf-section.s-card-border"
-const price = ss[8].childNodes[2].childNodes[2].childNodes[1].innerText;
-price.split(" ",1);
-
-if(arr[4].hasChildNodes()){
-  
-  const imgA = arr[4].children[1];
-  imgA.children[0].children[0].children[0].children[0].currentSrc;
-}
-
-// let imgA = arr[4].children[1];
-const ss = document.querySelectorAll(".a-section.a-spacing-base");
-let arr = [...ss];
-for (let i = 0; i < arr.length; i++) {
-  let imgA = arr[i].children[1];
-  imgA.children[0].children[0].children[0].children[0].currentSrc
-}
-// arr.forEach(it=> console.log(it.children[1]))
-
-arr.forEach(it=> {
-  let data = it.children[1];
- let imgAr = data.children[0].children[0].children[0].children[0].currentSrc
-console.log(imgAr)
-})
-  
-  
