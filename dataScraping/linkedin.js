b2668d910f6cfba69905c096bea49902adee788b
Dinesh kumar <dineshsoni8569@gmail.com>
2023-09-25 17:54:46 +0530

    more updations

--- dataScraping/linkedin.js
+++ dataScraping/linkedin.js
@@ -1,5 +1,57 @@
 const list = document.querySelectorAll(".discover-fluid-entity-list--item");
-list.forEach((card)=>{
-    const userCard = card.querySelector('.ember-view');
-    console.log(userCard)
+list.forEach((card) => {
+  const userCard = card.querySelector(".ember-view");
+  console.log(userCard);
 });
+
+const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+list.forEach((card) => {
+  const userCard = card.querySelector(".ember-view");
+  const info = ["software engineer", "Developer", "sde", "student"];
+  const occupation = userCard.querySelector(
+    ".discover-person-card__occupation"
+  );
+  const occuInfo = occupation.textContent;
+  console.log(occuInfo.includes(info));
+});
+
+const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+list.forEach((card) => {
+  const userCard = card.querySelector(".ember-view");
+  const info = ["software engineer", "developer", "sde", "student"];
+  const occupation = userCard.querySelector(
+    ".discover-person-card__occupation"
+  );
+  const occuInfo = occupation.textContent.toLowerCase();
+  console.log(info);
+  console.log(occuInfo.trim().split(" ").includes(info));
+});
+
+const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+list.forEach((card) => {
+  const userCard = card.querySelector(".ember-view");
+  const info = ["software engineer", "developer", "sde", "student"];
+  const occupation = userCard.querySelector(
+    ".discover-person-card__occupation"
+  );
+  const occuInfo = occupation.textContent.toLowerCase().trim();
+  info.forEach((keyword) => {
+    console.log(occuInfo, occuInfo.includes(keyword));
+  });
+});
+
+// console.log(info)
+// console.log(occuInfo.trim().includes(info));
+
+const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+list.forEach((card) => {
+  const userCard = card.querySelector(".ember-view");
+  const info = ["software engineer", "developer", "sde", "student"];
+  const occupation = userCard.querySelector(
+    ".discover-person-card__occupation"
+  );
+  const occuInfo = occupation.textContent.toLowerCase().trim().split(" ");
+  info.forEach((keyword) => {
+    console.log(occuInfo, occuInfo.includes(`${"software engineer","software developer", "sde", "student"}`));
+  });
+}); 
