f675676be4bfb6a25958235baef7e231092cc425
Dinesh kumar <dineshsoni8569@gmail.com>
2023-10-08 14:30:48 +0530

    linkedin scroll

--- dataScraping/linkedin.js
+++ dataScraping/linkedin.js
@@ -14,7 +14,16 @@ list.forEach((card) => {
       }
   });
 }); 
-
+const scrollUser = ()=>{
+  const scrollInterval = setInterval(() => {
+    window.scrollBy(0,100);
+  }, 100);
+  
+  setTimeout(() => {
+    clearInterval(scrollInterval) 
+  }, 3000);
+}
+scrollUser();
 const list = document.querySelectorAll(".discover-fluid-entity-list--item");
 list.forEach((card) => {
   const userCard = card.querySelector(".ember-view");
@@ -25,18 +34,27 @@ list.forEach((card) => {
    const occuInfo = occupation.textContent.toLowerCase().trim();
   info.forEach((word) => {
       if(occuInfo.includes(word)){
-             const connectButton = card.querySelector(".discover-entity-type-card__bottom-container button");
+           const connectButton = card.querySelector(".discover-entity-type-card__bottom-container button");
    console.log(connectButton);
     // connectButton.click()
       }
   });
 }); 
-
-const scrollFunc = () => {
-  const list = document.querySelectorAll(".artdeco-modal__content.discover-cohort-recommendations-modal__content");
-  setTimeout(() => { 
-    list.scrollTop -= 10;
-  }, 1000);
-}
-scrollFunc();
-
+const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+list.forEach((card) => {
+  const userCard = card.querySelector(".ember-view");
+  const info = ["software" , "engineer", "developer", "sde", "student"];
+  const occupation = userCard.querySelector(
+    ".discover-person-card__occupation"
+  );
+   const occuInfo = occupation.textContent.toLowerCase().trim();
+  info.forEach((word) => {
+      if(occuInfo.includes(word)){
+           let connectButton = card.querySelector(".discover-entity-type-card__bottom-container button");
+            if(connectButton.textContent = 'Connect'){
+             console.count(connectButton.length);
+            }
+    // connectButton.click()
+      }
+  });
+}); 
