32e18a4a02e1f0a5898316e0a6ce13fdde4fe770
Dinesh kumar <dineshsoni8569@gmail.com>
2023-10-08 14:55:46 +0530

    2nd linkedin

--- dataScraping/linkedin.js
+++ dataScraping/linkedin.js
@@ -1,60 +1,54 @@
-const list = document.querySelectorAll(".discover-fluid-entity-list--item");
-list.forEach((card) => {
+//1st function
+const firstFunction = ()=>{
+
+  const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+  list.forEach((card) => {
   const userCard = card.querySelector(".ember-view");
-  const info = ["software" , "engineer", "developer", "sde", "student"];
+  const info = ["software", "engineer", "developer", "sde", "student"];
   const occupation = userCard.querySelector(
     ".discover-person-card__occupation"
   );
-   const occuInfo = occupation.textContent.toLowerCase().trim();
+  const occuInfo = occupation.textContent.toLowerCase().trim();
   info.forEach((word) => {
-      if(occuInfo.includes(word)){
-             const connectButton = card.querySelector(".discover-entity-type-card__bottom-container button");
-   console.log(connectButton);
-    // connectButton.click()
-      }
+    if (occuInfo.includes(word)) {
+      const connectButton = card.querySelector(
+        ".discover-entity-type-card__bottom-container button"
+      );
+      console.log(connectButton);
+      // connectButton.click()
+    }
   });
-}); 
-const scrollUser = ()=>{
-  const scrollInterval = setInterval(() => {
-    window.scrollBy(0,100);
-  }, 100);
-  
-  setTimeout(() => {
-    clearInterval(scrollInterval) 
-  }, 3000);
+});
 }
-scrollUser();
-const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+firstFunction();
+//2nd function
+const secondFunc = ()=>{
+  const list = document.querySelectorAll(".discover-fluid-entity-list--item");
+  const scrollUser = () => {
+  const scrollInterval = setInterval(() => {
+    window.scrollBy(0, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(scrollInterval);
+  }, 3000);
+};
 list.forEach((card) => {
   const userCard = card.querySelector(".ember-view");
-  const info = ["software" , "engineer", "developer", "sde", "student"];
+  const info = ["software", "engineer", "developer", "sde", "student"];
   const occupation = userCard.querySelector(
     ".discover-person-card__occupation"
   );
-   const occuInfo = occupation.textContent.toLowerCase().trim();
+  const occuInfo = occupation.textContent.toLowerCase().trim();
   info.forEach((word) => {
-      if(occuInfo.includes(word)){
-           const connectButton = card.querySelector(".discover-entity-type-card__bottom-container button");
-   console.log(connectButton);
-    // connectButton.click()
-      }
+    if (occuInfo.includes(word)) {
+      let connectButton = card.querySelector(
+        ".discover-entity-type-card__bottom-container button"
+      );
+      // if ((connectButton.textContent = "Connect")) {}
+      // connectButton.click()
+    }
   });
-}); 
-const list = document.querySelectorAll(".discover-fluid-entity-list--item");
-list.forEach((card) => {
-  const userCard = card.querySelector(".ember-view");
-  const info = ["software" , "engineer", "developer", "sde", "student"];
-  const occupation = userCard.querySelector(
-    ".discover-person-card__occupation"
-  );
-   const occuInfo = occupation.textContent.toLowerCase().trim();
-  info.forEach((word) => {
-      if(occuInfo.includes(word)){
-           let connectButton = card.querySelector(".discover-entity-type-card__bottom-container button");
-            if(connectButton.textContent = 'Connect'){
-             console.count(connectButton.length);
-            }
-    // connectButton.click()
-      }
-  });
-}); 
+});
+}
+secondFunc();
